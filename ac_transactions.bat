7f12e5cd25c5d823b1b53fd2e2f4bb52dd3e90da
Timothy Lin <Tim@ad.forteresources.com>
2024-09-04 17:20:17 -0700

    Modify bat file

--- ac_transactions.bat
+++ ac_transactions.bat
@@ -1,3 +1,4 @@
 @echo off
-call conda activate qb_extract
-python C:\Users\Tim\Documents\projects\qb_extract\transactions.py
+@REM call conda activate qb_extract
+@REM python C:\Users\Tim\Documents\projects\qb_extract\transactions.py
+C:/Users/tim/AppData/Local/miniconda3/envs/qb_extract/python.exe c:/Users/tim/github/qb_extract/transactions.py
