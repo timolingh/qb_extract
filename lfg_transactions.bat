428a6e79e0e7d184cb9f90fc79bf887268c31a67
Timothy Lin <Tim@ad.forteresources.com>
2024-10-29 16:58:17 -0700

    Add bat file for lfg_transactions

---
+++ lfg_transactions.bat
@@ -0,0 +1,4 @@
+@echo off
+@REM call conda activate qb_extract
+@REM python C:\Users\Tim\Documents\projects\qb_extract\transactions.py
+C:/Users/tim/AppData/Local/miniconda3/envs/qb_extract/python.exe c:/Users/tim/github/qb_extract/lfg_transactions.py
